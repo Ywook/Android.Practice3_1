ab058879bd028447de253aa7340fd86c90f91c7e
조영욱 <Mac@YoungWook-MacBook-Pro.local>
2017-03-23 17:00:20 +0900

    면적 계산기 계산 구현 덜햇음

---
+++ app/src/main/java/com/examples/ss3_2/MainActivity.java
@@ -0,0 +1,115 @@
+package com.examples.ss3_2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import static java.lang.Double.parseDouble;
+
+public class MainActivity extends AppCompatActivity {
+    TabHost tabHost;
+
+    Button btn1, btn2, btn3;
+
+    EditText editText1, editText2, editText3;
+
+    TextView textView1, textView2;
+
+    double Official = 3.3058;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        tabHost = (TabHost) findViewById(R.id.tabhost1);
+        tabHost.setup();
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("1").setContent(R.id.tab1).setIndicator("BMI 계산기");
+
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("2").setContent(R.id.tab2).setIndicator("면적 계산기");
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+
+    }
+
+    void init(){
+
+        editText1 = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+
+        textView1 = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        btn1 = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button3);
+        btn3 = (Button) findViewById(R.id.button4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BMIcheck();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToMeter();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToPyeong();
+            }
+        });
+    }
+
+    void BMIcheck(){
+        String s1, s2;
+
+        s1 = editText1.getText().toString();
+        s2 = editText2.getText().toString();
+
+        int kilo = (int)Math.pow(parseDouble(s2)/100,2);
+
+        double result = parseDouble(s1)/kilo;
+
+        if(result < 18.5){
+            textView1.setTextColor(Color.RED);
+            textView1.setText("체중부족!!!!");
+        }else if(result <22.9){
+            textView1.setTextColor(Color.GREEN);
+            textView1.setText("정상!!!!");
+        }else if(result <24.9){
+            textView1.setTextColor(Color.RED);
+            textView1.setText("과체중!!!!");
+        }else if(result >25){
+            textView1.setTextColor(Color.RED);
+            textView1.setText("비만입니다!!!");
+        }
+    }
+
+    void changeToMeter(){
+        String s1 = editText2.getText().toString();
+        double result = Math.round(Integer.parseInt(s1)*Official*10000)/10000;
+        textView2.setText(result + " 제곱미터");
+
+    }
+
+    void changeToPyeong(){
+        String s1 = editText3.getText().toString();
+
+        double result = parseDouble(s1)/Official;
+        textView2.setText(result+" 평");
+    }
+}
